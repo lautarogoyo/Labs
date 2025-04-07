f47fbb9e693fd2a50573f200f8358a85f8ba2604
lautarogoyo <127627677+lautarogoyo@users.noreply.github.com>
2025-04-07 17:45:00 -0300

    Unidad 1 hecha

---
+++ Unidad.1.Capitulo.2.Lab.1.IDE/Lab01/Console/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteALine("Esta linea va a dar error!");
+        }
+    }
+}
